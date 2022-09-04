892e8331c42afba2d871caeaf1d5554328f545cc
devramcc <dev.ramddd@gmail.com>
2022-09-04 21:52:38 +0700

    adding menu_controller

---
+++ lib/controllers/menu_controller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:web_dashboard/constants/style.dart';
+import 'package:web_dashboard/routing/routes.dart';
+
+class MenuController extends GetxController{
+  static MenuController instance = Get.find()
+  var activeItem = OverViewPageRoute.obs;
+  var hoverItem = "".obs;
+
+  changeActiveitemTo(String itemName){
+    activeItem.value = itemName;
+  }
+
+  onHover(String itemName) {
+    if(!isActive(itemName)) hoverItem.value = itemName;
+  }
+
+  isActive(String itemName) => activeItem.value == itemName;
+
+  isHovering(String itemName) => hoverItem.value == itemName;
+
+  Widget returnIconFor(String itemName) {
+    switch (itemName) {
+      case OverViewPageRoute:
+        return _CustomIcon(Icons.trending_up, itemName);
+      case DriversPageRoute:
+        return _CustomIcon(Icons.drive_eta, itemName);
+      case ClientsPageRoute:
+        return _CustomIcon(Icons.people_alt_outlined, itemName);
+      case AuthenticationPageRoute:
+        return _CustomIcon(Icons.exit_to_app, itemName);
+      default:
+        return _CustomIcon(Icons.exit_to_app, itemName);
+    };
+  }
+
+  Widget _CustomIcon(IconData icon, String itemName) {
+    if(isActive(itemName)) return Icon(icon, size: 22, color: dark,);
+
+    return Icon(icon, color: isHovering(itemName) ? dark : lightGrey,);
+  }
+}
---
+++ lib/routing/routes.dart
@@ -0,0 +1,11 @@
+const OverViewPageRoute = "Overview";
+const DriversPageRoute = "Drivers";
+const ClientsPageRoute = "Clients";
+const AuthenticationPageRoute = "Aunthentication";
+
+List SideMenuItems = [
+ OverViewPageRoute,
+ DriversPageRoute,
+ ClientsPageRoute,
+ AuthenticationPageRoute
+];
